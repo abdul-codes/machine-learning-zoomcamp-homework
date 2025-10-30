3e0ad7e94ab7cd3156ddb7b1069bdf469a3caf16
abdul-codes <abdulsalamkinsanya2@gmai.com>
2025-10-30 16:21:31 +0100

    ``` feat: Add Python version, pyproject and FastAPI example ``` feat: Add churn prediction app and marketing script ```

---
+++ workshop-fastapi/marketing.py
@@ -0,0 +1,37 @@
+
+
+import requests
+
+url = 'http://localhost:9096/predict'
+
+
+customer = {
+  "gender": "male",
+  "seniorcitizen": 0,
+  "partner": "yes",
+  "dependents": "no",
+  "phoneservice": "no",
+  "multiplelines": "no_phone_service",
+  "internetservice": "dsl",
+  "onlinesecurity": "no",
+  "onlinebackup": "yes",
+  "deviceprotection": "no",
+  "techsupport": "no",
+  "streamingtv": "no",
+  "streamingmovies": "no",
+  "contract": "month-to-month",
+  "paperlessbilling": "yes",
+  "paymentmethod": "electronic_check",
+  "tenure": 6,
+  "monthlycharges": 29.85,
+  "totalcharges": 129.85
+}
+response = requests.post(url, json=customer)
+churn = response.json()
+
+print(" response= ",churn)
+
+if churn['churn'] >= 0.5:
+    print("send email with promo")
+else:
+    print("Do not do anything")
